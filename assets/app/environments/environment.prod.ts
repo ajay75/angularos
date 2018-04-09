c24d8afd8e51f3ee54a937b12a84b5afd08ecdad
ajay75 <ajay75@users.noreply.github.com>
2018-04-08 21:07:47 -0400

    local mac link

--- assets/app/environments/environment.prod.ts
+++ assets/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://todolist-mde.1d35.starter-us-east-1.openshiftapps.com'
+  apiUrl: 'http://todolist-backend.192.168.64.2.nip.io'
 };
