5df767adb26c8e27ea102009fabc302cfae3d701
ajay75 <ajay75@users.noreply.github.com>
2019-02-26 16:14:54 -0500

    update to new API server

--- assets/app/environments/environment.prod.ts
+++ assets/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://todolist-jenkins-tester.b9ad.pro-us-east-1.openshiftapps.com/'
+  apiUrl: 'http://todolist-mde.7e14.starter-us-west-2.openshiftapps.com/'
 };
